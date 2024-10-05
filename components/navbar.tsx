564a42ed6f54faf270188e601b613cb8c61a2b27
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2024-10-05 18:11:52 +0100

    Commit

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import Button from './button';
 export default function Navbar() {
   return (
     <div className={styles.nav}>
+      <p style={{ color: 'var(--white)', fontSize: 20 }}>PackPal.AI</p>
       <Button text={'Try now!'} type="secondary" />
     </div>
   );
